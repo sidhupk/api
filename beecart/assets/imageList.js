6c9d7e8db1cb94169e8b97c07834e75bab640a4e
Sidhupk <sidhupkonline@gmail.com>
2021-05-31 12:28:49 +0530

    Update imageList.js

--- beecart/assets/imageList.js
+++ beecart/assets/imageList.js
@@ -1,14 +1,17 @@
-response : [
+function  getImageList() {
+    return [
       {
-        "url": "https://github.com/sidhupk/api/raw/main/beecart/assets/offer1.jpg",
-        "link": "https://mydukaan.io/kunjipaka/categories/sweets"
+        url: 'https://github.com/sidhupk/api/raw/main/beecart/assets/offer1.jpg',
+        link: 'https://mydukaan.io/kunjipaka/categories/sweets'
       },
       {
-        "url": "https://github.com/sidhupk/api/raw/main/beecart/assets/offer2.jpg",
-        "link": "https://mydukaan.io/kunjipaka/products/vicks"
+        url: 'https://github.com/sidhupk/api/raw/main/beecart/assets/offer2.jpg',
+        link: 'https://mydukaan.io/kunjipaka/products/vicks'
       },
       {
-        "url": "https://github.com/sidhupk/api/raw/main/beecart/assets/offer3.jpg",
-        "link": "https://mydukaan.io/kunjipaka/categories/rice"
+        url: 'https://github.com/sidhupk/api/raw/main/beecart/assets/offer3.jpg',
+        link: 'https://mydukaan.io/kunjipaka/categories/rice'
       },
-]
+    
+    ]; 
+   }
