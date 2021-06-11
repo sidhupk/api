ff028fc5f7bc38f1a08a4f52882d26e4862b8218
sidhupk <37614876+sidhupk@users.noreply.github.com>
2021-06-11 21:19:45 +0530

    Update imageList.js

--- 247cart/assets/imageList.js
+++ 247cart/assets/imageList.js
@@ -3,15 +3,6 @@ function  getImageList() {
       {
         url: 'https://raw.githubusercontent.com/beecart/api/main/images/Frame-871-393x200.jpg',
         link: 'https://beecart.online/beecart/products/sambar-kit-2kg-sambar-powder-100gm'
-      },
-      {
-        url: 'https://github.com/beecart/api/raw/main/images/IMG_20200518_144904.jpg' ,
-        link: 'https://mydukaan.io/beecart/products/parle-real-elaichi-premium-rusk-300g'
-      },
-      {
-        url: 'https://github.com/beecart/api/raw/main/images/download.jpg',
-        link: 'https://beecart.online/beecart/products/sun-plus-aquamatic-detergent-powder-4-2kg-offer-pack'
-      },
-    
+      }    
     ]; 
    }
