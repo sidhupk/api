f59ca3c2ee72ffd07ce7c4eea8c04a328d980498
sidhupk <37614876+sidhupk@users.noreply.github.com>
2021-06-12 14:23:14 +0530

    Update imageList.js

--- 247cart/assets/imageList.js
+++ 247cart/assets/imageList.js
@@ -1,8 +1,12 @@
 function  getImageList() {
     return [
       {
-        url: 'https://raw.githubusercontent.com/beecart/api/main/images/Frame-871-393x200.jpg',
-        link: 'https://beecart.online/beecart/products/sambar-kit-2kg-sambar-powder-100gm'
+        url: 'https://github.com/sidhupk/api/raw/main/247cart/assets/images/offer2.jpg',
+        link: '#'
+      },
+      {
+        url: 'https://github.com/sidhupk/api/raw/main/247cart/assets/images/offer3.jpg',
+        link: '#'
       }    
     ]; 
    }
