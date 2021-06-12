542d0495cd38aa167360c6f69a9cbd53314c8ba8
sidhupk <37614876+sidhupk@users.noreply.github.com>
2021-06-12 14:37:44 +0530

    Update custom.js

--- 247cart/assets/custom.js
+++ 247cart/assets/custom.js
@@ -49,7 +49,7 @@ function setPage() {
         parentDiv.insertBefore(newLink, searchProdDiv);        
 
         var linkParent = parentDiv.getElementsByClassName('cover-pic-link')[0];
-        linkParent.innerHTML = "<img src='"+randomItem.url+"' />";
+        linkParent.innerHTML = "<img src='"+randomItem.url+"' style='margin-top: 10px; '/>";
      }
    }
    setPage();
