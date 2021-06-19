7c8e6c3bc2c72af79acbf49e3742d8090369cbde
sidhupk <37614876+sidhupk@users.noreply.github.com>
2021-06-19 13:01:47 +0530

    Update imageList.js

--- 247cart/assets/imageList.js
+++ 247cart/assets/imageList.js
@@ -1,5 +1,12 @@
 function  getImageList() {
     return [
-         
+         {
+        url: 'https://github.com/sidhupk/api/raw/main/247cart/assets/images/offer2.jpg',
+        link: '#'
+      },
+      {
+        url: 'https://github.com/sidhupk/api/raw/main/247cart/assets/images/offer3.jpg',
+        link: '#'
+      } 
     ]; 
    }
