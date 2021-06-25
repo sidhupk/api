b661a71f768d00dee1d365d95911a8204a1bb3b4
sidhupk <37614876+sidhupk@users.noreply.github.com>
2021-06-25 14:09:10 +0530

    Update custom.js

--- cowin/custom.js
+++ cowin/custom.js
@@ -96,7 +96,7 @@ var snd = new Audio("data:audio/wav;base64,UklGRjz3AQBXQVZFZm10IBAAAAABAAEAEnoAA
         a.style.color = '#002060!important';
         a.style.fontWeight = 600;
         a.setAttribute('id','shareApp');
-        a.style.display = 'none';
+        // a.style.display = 'none';
         a.setAttribute('href','https://web.whatsapp.com/send?text=Download Cowin India Slot Alert App Here - https://play.google.com/store/apps/details?id=com.cowinindiaslotalert.sidhupk');
         document.getElementsByClassName('mainContainer')[0].insertBefore(a, document.getElementsByTagName('router-outlet')[0]);
       }
