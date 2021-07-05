80057062e380072230f7c4b4cf8e1a0111bd0a63
sidhupk <37614876+sidhupk@users.noreply.github.com>
2021-07-05 18:47:10 +0530

    Update custom_new.js

--- cowin/custom_new.js
+++ cowin/custom_new.js
@@ -8,6 +8,7 @@ var isAlamOn = true;
 var isFilterChanged = true;
 var counter;
 var jsVersion = 4;   
+alert(jsVersion);
    function triggerApi() {
     document.getElementsByClassName('pin-search-btn')[0].click();
    }
