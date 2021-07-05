65d863916fb21ac588919efdaeee5f9886aa335d
sidhupk <37614876+sidhupk@users.noreply.github.com>
2021-07-05 14:04:08 +0530

    Update custom.js

--- cowin/custom.js
+++ cowin/custom.js
@@ -260,7 +260,7 @@ setTimeout(function() {
 setInterval(()=> {
 if(document.readyState === "complete" && window.location.href.indexOf('notfound') === -1 ){
 	 if(document.getElementsByClassName('noteTextMobile').length > 0) {
-			  // document.getElementsByClassName('noteTextMobile')[0].remove();
+		document.getElementsByClassName('noteTextMobile')[0].remove();
 	}
 	if(document.getElementById('Search-Vaccination-Center') !== null && document.getElementsByTagName("html")[0].getAttribute('init') === null) {
 		
