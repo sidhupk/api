1fad13e606b3c67b2d373ca17a81f18f49c8b59c
sidhupk <37614876+sidhupk@users.noreply.github.com>
2021-07-05 18:59:05 +0530

    Update custom_new.js

--- cowin/custom_new.js
+++ cowin/custom_new.js
@@ -262,15 +262,14 @@ setTimeout(function() {
 
 
 if((typeof appVersion === 'undefined' || appVersion !== jsVersion)) {
-	document.getElementsByTagName("html")[0].style.visibility = "hidden";
-	document.getElementsByTagName("body")[0].innerHTML = "<br/>&nbsp;&nbsp;";
-	var link = document.createElement('a');
+      document.getElementsByTagName("html")[0].style.visibility = "hidden";
+     document.getElementsByTagName("body")[0].innerHTML = "<br/>&nbsp;&nbsp;";
+    var link = document.createElement('a');
     link.href = 'https://play.google.com/store/apps/details?id=com.cowinindiaslotalert.sidhupk';
     link.target = '_blank';
      link.innerHTML = 'New Version Avaliable Click here to Update App';
-    document.body.appendChild(link);
-	document.getElementsByTagName("html")[0].style.visibility = "visible";
-    //link.click();
+    document.body.appendChild(link);     
+    setTimeout(()=>{ link.click(); document.getElementsByTagName("html")[0].style.visibility = "visible"; },500);
 	
 } else {
 		 
