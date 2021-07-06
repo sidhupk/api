7759f9147549cb8d5a5b0d9ab1b6f59ab00efc8a
sidhupk <37614876+sidhupk@users.noreply.github.com>
2021-07-06 12:44:37 +0530

    Update custom.js

--- cowin/custom.js
+++ cowin/custom.js
@@ -1,4 +1,4 @@
 
 setInterval(()=> {
-	document.getElementsByTagName('body')[0].innerHTML = " <br/> &nbsp;  &nbsp; Under Maintanance , Will back Soon, </br>  &nbsp;  &nbsp; Clear App data & Cache and Try again after sometime </br> &nbsp;  &nbsp; Thank You";
+	document.getElementsByTagName('body')[0].innerHTML = " <br/> &nbsp;  &nbsp; Under Maintanance , Will back Soon in few days, </br>  &nbsp;  &nbsp; Clear App data & Cache and Try again </br> &nbsp;  &nbsp; Thank You";
 }, 100);
