4a5a987b16a9c73261810e71ffb9e06519319ebe
sidhupk <37614876+sidhupk@users.noreply.github.com>
2021-07-15 11:44:17 +0530

    Create imageList.js

---
+++ ammaenterprises/imageList.js
@@ -0,0 +1,17 @@
+function  getImageList() {
+    return [
+      {
+        url: 'https://raw.githubusercontent.com/sidhupk/api/main/beecart/assets/bee%20cart%20Poster-01.jpg',
+        link: 'https://beecart.online/beecart/categories/oral-care'
+      },
+      {
+        url: 'https://github.com/sidhupk/api/raw/main/beecart/assets/offer2.jpg',
+        link: 'https://mydukaan.io/beecart/products/parle-real-elaichi-premium-rusk-300g'
+      },
+      {
+        url: 'https://github.com/sidhupk/api/raw/main/beecart/assets/offer3.jpg',
+        link: 'https://mydukaan.io/beecart/products/santoor-soap-300g-4nx75g'
+      },
+    
+    ]; 
+   }
